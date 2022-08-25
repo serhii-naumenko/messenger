6a1c80274b7cb9089c58bbc3186f38d7090de36e
serhii_naumenko <s.naumenko.miy@gmail.com>
2022-08-25 19:39:08 +0300

    correct path in Route

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,14 @@ export const App: React.FC = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<LoginPage />} />
+        <Route path={`${process.env.PUBLIC_URL}/messenger`} element={<Messenger />} />
+        <Route path={`${process.env.PUBLIC_URL}/register`} element={<RegisterPage />} />
+        <Route path={`${process.env.PUBLIC_URL}*`} element={<NotFound />} />
+        {/* <Route path="/" element={<LoginPage />} />
         <Route path="/messenger" element={<Messenger />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </div>
   );
